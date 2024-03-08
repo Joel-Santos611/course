926b40eb455f7b58766e7e65ecac9e41c6ec58e8
Joel-santos611 <joeleduardodossantos611@gmail.com>
2024-03-08 03:44:09 +0100

    project started

---
+++ src/main/java/com/educandoweb/course/model/User.java
@@ -0,0 +1,12 @@
+package com.educandoweb.course.model;
+
+public class User {
+private Long id;
+private String nome;
+private String email;
+private String phone;
+private String password;
+
+
+
+}
